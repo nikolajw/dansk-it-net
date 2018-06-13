147bd7761e99004f32f6bad9a3dcc6cab3eee052
Nikolaj Winnes <nikolaj.winnes@storecast.de>
2018-06-13 14:12:50 +0200

    Changed values

--- Booking/Controllers/ValuesController.cs
+++ Booking/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace Booking.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value3", "value2" };
         }
 
         // GET api/values/5
